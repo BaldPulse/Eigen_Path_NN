be5ae65592dbf81b26cb48bd7b0bec3e075b2108
Zhao Tang <zhaotang@localhost.localdomain>
2020-04-17 12:39:27 -0700

    updated sgc encoder

--- gcn/sgc_autoencoder/sgc_autoencoder.py
+++ gcn/sgc_autoencoder/sgc_autoencoder.py
@@ -17,7 +17,7 @@ def sgc_layer(numhop, W, A, H):
     layer = tf.matmul(layer, W)
     return layer
 
-def sgc_autoencoder(X, We, Wd, A):
+def sgc_autoencoder(X, We, Wx, Wk, Wd, A):
     '''
     input:
     X: f*n array of flows
@@ -25,8 +25,10 @@ def sgc_autoencoder(X, We, Wd, A):
     output:
     X': f*n array
     '''
-    E = sgc_layer(numhop=4, W = We, A=A, H=X)
-    x_bar = tf.matmul(E, Wd)
+    I = sgc_layer(numhop=4, W = We, A=A, H=X)
+    E = tf.matmul(Wx, I)
+    E = tf.nn.relu(tf.math.subtract(E, Wk))
+    x_bar = tf.transpose(tf.matmul(E, Wd))
     return x_bar
 
 def load_data(path):
@@ -38,23 +40,24 @@ path = '/home/zhaotang/Documents/eigen_path/gcn/data/3/'
 
 flows, edge_adj = load_data(path)
 n_edges = flows.shape[1]
-n_flows = flows.shape[0]
+n_flows = 1
 n_paths    = 1
-flows = flows.T
 print('H dim', flows.shape)
 weights = {
     'encoder': tf.Variable(tf.random_normal([n_flows, n_paths])),
-    'decoder': tf.Variable(tf.random_normal([n_paths, n_flows]))
+    'extra':   tf.Variable(tf.random_normal([n_flows, n_edges])),
+    'const':   tf.Variable(tf.random_normal([n_flows, n_paths])),
+    'decoder': tf.Variable(tf.random_normal([n_paths, n_edges]))
 }
 
-training_epochs = 3000000
-batch_size = flows.shape[1]
+training_epochs = 3000
+batch_size = 1
 dropout = tf.placeholder("float")
 
 X = tf.placeholder("float", [n_edges, n_flows])
 A = tf.constant(prepare_adj(edge_adj), dtype = "float" , shape = [n_edges, n_edges])
 
-pred_flows = sgc_autoencoder(X, weights['encoder'], weights['decoder'], A)
+pred_flows = sgc_autoencoder(X, weights['encoder'], weights['extra'], weights['const'], weights['decoder'], A)
 loss       = tf.nn.l2_loss(tf.math.subtract(pred_flows, X))
 optimizer  = tf.train.AdamOptimizer(learning_rate=0.0003).minimize(loss)
 We_print   = tf.print("encoder:", weights['encoder'], output_stream=sys.stdout)
@@ -66,30 +69,22 @@ Wd_print   = tf.print("decoder:", weights['decoder'], output_stream=sys.stdout)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    
+    prev_loss = 0.1
     for epoch in range(training_epochs):
-        '''
         avg_loss = 0.0
         total_batch = int(len(flows) / batch_size)
         for i in range(total_batch-1):
-            batch_X = flows[i*32:(i+1)*32]
+            batch_X = flows[i*batch_size:(i+1)*batch_size]
             _, c = sess.run([optimizer, loss], 
                             feed_dict={
-                                X: batch_X,
+                                X: batch_X.T,
                                 dropout: 0.2
                             })
             avg_loss += c / total_batch
-        '''
-        _, c = sess.run([optimizer, loss],
-                        feed_dict={
-                                X: flows,
-                                dropout: 0.2
-                            })
-        avg_loss = c
-        print("Epoch:", '%04d' % (epoch+1), "loss=", \
-                "{:.9f}".format(avg_loss))
-        if(avg_loss < 0.5):
+        print('epoch', epoch,'avgloss', avg_loss)
+        if(avg_loss < 0.5) and epoch > 1:
             break
+        prev_loss = avg_loss
     sess.run([We_print, Wd_print])
     
     '''
