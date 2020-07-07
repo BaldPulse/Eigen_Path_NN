a604ff471d963a1b14f8956399a8b9e0db50ad34
Zhao Tang <tangzhao1999@hotmail.com>
2020-07-07 10:30:50 -0700

    rearraged code for testing

--- gcn/sgc_autoencoder/sgc_autoencoder_keras.py
+++ gcn/sgc_autoencoder/sgc_autoencoder_keras.py
@@ -5,6 +5,7 @@ import os
 import tensorflow as tf
 import numpy as np
 import numpy.linalg as npla
+import sys, getopt
 from sgc_util_keras import *
 
 def prepare_adj(adj):
@@ -40,14 +41,11 @@ def get_sgc_model(num_nodes=41, num_sgc_feats=32, latent_size=1):
     
     latent = tf.keras.layers.Dense(latent_size,
                                    activation='relu',
+                                   #kernel_initializer=tf.keras.initializers.GlorotNormal(seed=None),
                                    kernel_initializer=tf.keras.initializers.RandomUniform(minval=0., maxval=1.),
                                    name='bottleneck')(sgc_out)
 
-    
-    # Consider adding `kernel_regularizer=tf.keras.regularizers.l1()` to below,
-    # to encourage sparser (mostly zero) weights for the decoder.
-    #decode = tf.keras.layers.Dense(num_nodes, kernel_constraint=tf.keras.constraints.NonNeg(), kernel_regularizer=tf.keras.regularizers.l1(0.00), use_bias=False, name='decoder')(latent)
-    decode = sgc_decoder(num_nodes, name = 'decoder', kernel_regularizer = l1l2_corr_sm(l1 = 0.0, l2 = 0., kc = 0.0, ks = 0.2, kv = 0.2))(latent)
+    decode = sgc_decoder(num_nodes, name = 'decoder', kernel_regularizer = l1l2_corr_sm(l1 = 0.0, l2 = 0., kc = 1.5, ks = 0.2, kv = 0.2))(latent)
     decode = tf.keras.layers.Reshape((num_nodes, 1))(decode)
     
     model = tf.keras.Model(inputs=(I, A), outputs=decode)
@@ -68,26 +66,58 @@ def load_data(path):
         print('no noiseless flows in'+path)
     return flows, noiseless_flows, edge_adj, expected_paths
 
+optlist, args = getopt.getopt(sys.argv[1:], 'n:p:', ['noiseless', 'data='])
 
+learning_rate = 0.0003
+batch_size = 32
+n_epochs = 300
+
+
+_log_dir = './logs'
+
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=_log_dir, write_images=False, histogram_freq=1)
+tbi_callback = TensorBoardImage('Image Example', log_dir = _log_dir)
+earlystop_callback = EarlyStoppingByLossVal(monitor='loss', value=3, verbose=0)
+time_callback = TimeHistory()
 
 path = '../data/7/'
-val_path = '../data/5_val/'
+
+_callbacks = []
+use_noiseless = False
+_npath = -1
+for a,o in optlist:
+    if a=="-n":
+        n_epochs=int(o)
+    if a=='--noiseless':
+        use_noiseless = True
+    if a=='--data':
+        path=o
+    if a=='--tensorboard':
+        _callback.append(tensorboard_callback)
+    if a=='--tbi':
+        _callback.append(tbi_callback)
+    if a=='--earlystop':
+        _callback.append(earlystop_callback)
+    if a=='-p':
+        _npath = int(o)
+
 flows, noiseless_flows, edge_adj, expected_paths = load_data(path)
-val_flows, val_noiseless_flows, _, _ = load_data(val_path)
 flows = np.expand_dims(flows, 2)
-val_flows = np.expand_dims(val_flows, 2)
 noiseless_flows = np.expand_dims(noiseless_flows, 2)
 A = prepare_adj(edge_adj)
 A = np.tile(np.expand_dims(A, 0), (flows.shape[0], 1, 1))  # Adding a dummy batch dimension
+
 
 n_edges = flows.shape[1]
 n_sgc_feats = 32
 n_flows = 5
 n_paths = expected_paths.shape[0]
+_flows = flows
+if use_noiseless:
+    _flows = noiseless_flows
+if _npath >0:
+    n_paths = _npath
 
-learning_rate = 0.0003
-batch_size = 32
-n_epochs = 300
 
 model = get_sgc_model(n_edges, 
                       num_sgc_feats=n_sgc_feats, 
@@ -96,35 +126,22 @@ model = get_sgc_model(n_edges,
 model.compile(
     optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
     loss=tf.keras.losses.MSE,
-    # You could add metrics to track during training, 
-    # e.g. difference between the decoder weight and ground truth paths.
 )
 
-log_dir = './logs'
-
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, write_images=False, histogram_freq=1)
-tbi_callback = TensorBoardImage('Image Example', log_dir = log_dir)
-time_callback = TimeHistory()
-
-
-#os.system('rm -rf ' + log_dir)
 
 model.fit(
-    x={'input_node_features': noiseless_flows, 'adjacency': A},
-    y=noiseless_flows,
+    x={'input_node_features': _flows, 'adjacency': A},
+    y= _flows,
     batch_size=batch_size,
     epochs=n_epochs,
-    #callbacks=[tensorboard_callback, tbi_callback, time_callback],
-    #callbacks=[time_callback],
-    #validation_data=({'input_node_features': val_flows, 'adjacency': A},val_flows)
-    # you could also include logging to a file via callbacks,
-    # saving model to a file via callbacks, etc 
+    callbacks=_callbacks
 )
+
 
 
 e_shape = expected_paths.shape
 e_img = np.reshape(expected_paths, [1, e_shape[0], e_shape[1], 1])
-e_path = os.path.join(log_dir, 'e_paths')
+e_path = os.path.join(_log_dir, 'e_paths')
 file_writer = tf.summary.create_file_writer(e_path)
 with file_writer.as_default():
     tf.summary.image("Expected paths", e_img, step=0, description='expected paths to be learned')
@@ -134,8 +151,8 @@ print(model.summary())
 paths = model.get_layer('decoder').get_weights()
 now = datetime.now()
 l_shape = paths[0].shape
-l_img = np.reshape(tf.nn.softmax(paths[0])*10, [1, e_shape[0], e_shape[1], 1])
-l_path = os.path.join(log_dir, now.strftime("%H:%M:%S"))
+l_img = np.reshape(tf.nn.softmax(paths[0])*10, [1, n_paths, e_shape[1], 1])
+l_path = os.path.join(_log_dir, now.strftime("%H:%M:%S"))
 file_writer = tf.summary.create_file_writer(l_path)
 with file_writer.as_default():
     tf.summary.image("Learned paths", l_img, step=0, description='Learned paths from decoder')
--- gcn/sgc_autoencoder/sgc_util_keras.py
+++ gcn/sgc_autoencoder/sgc_util_keras.py
@@ -119,6 +119,23 @@ class TimeHistory(keras.callbacks.Callback):
         self.times.append(time.time() - self.epoch_time_start)
 
 
+class EarlyStoppingByLossVal(keras.callbacks.Callback):
+    def __init__(self, monitor='val_loss', value=0.00001, verbose=0):
+        super(keras.callbacks.Callback, self).__init__()
+        self.monitor = monitor
+        self.value = value
+        self.verbose = verbose
+
+    def on_epoch_end(self, epoch, logs={}):
+        current = logs.get(self.monitor)
+        if current is None:
+            print("Early stopping requires %s available!" % self.monitor)
+
+        if current < self.value:
+            if self.verbose > 0:
+                print("Epoch %05d: early stopping THR" % epoch)
+            self.model.stop_training = True
+
 class l1l2_corr_sm(regularizers.Regularizer):
     def __init__(self, l1=0., l2=0., kc = 0., ks = 0., kv = 0.):
         self.l1 = K.cast_to_floatx(l1)
