a6ff0c92e1ed6e105d235497c5793d398815602a
Zhao Tang <tangzhao1999@hotmail.com>
2020-09-01 23:38:51 -0700

    experiment setup

---
+++ gcn/sgc_autoencoder/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+edge=1
+path=2
+nl=0.3
+iter=500
+
+rm -rf ./logs/*
+
+while [ $edge -le 4 ]
+do
+    path=2
+    while [ $path -le 3 ]
+    do
+	echo python3 sgc_autoencoder_keras.py --data sfg/100/shared_edges/nl_${nl}/${edge}/${path}_pth --tensorboard -n ${iter} --name ${edge}e_${path}p
+	python3 sgc_autoencoder_keras.py --data sfg/100/shared_edges/nl_${nl}/${edge}/${path}_pth --tensorboard -n ${iter} --name ${edge}e_${path}p
+	python3 sgc_autoencoder_keras.py --data sfg/100/shared_edges/nl_${nl}/${edge}/${path}_pth --tensorboard -n ${iter} --name ${edge}e_${path}p
+	((path++))
+    done
+    rm -rf ../../results/shared_edges/nl_${nl}/${edge}e/*
+    echo cp -r ./logs/* ../../results/shared_edges/nl_${nl}/${edge}e/
+    cp -r ./logs/* ../../results/shared_edges/nl_${nl}/${edge}e/
+    rm -rf ./logs/
+    ((edge++))
+done
+
+
